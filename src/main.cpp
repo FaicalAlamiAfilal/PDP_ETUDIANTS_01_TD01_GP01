f9549dffe15f3c1be9a25197144153ca0c02e047
alamifaical <faicalalami99@gmail.com>
2024-06-12 00:13:01 +0200

    etape 2 final

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
-#include <Arduino.h>
+
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 33 
+
+#define DHTTYPE    DHT11    
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  // Initialise le capteur.
+  dht.begin();
 }
 
 void loop() {
-  // put your code here to run repeatedly
+
+  // Afficher la température avec un peu de formatage
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("capteur température:erreur de lecture"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Afficher l'humidité relative avec un peu de formatage
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("capteur humidité:erreur de lecture"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+    // Effectuer une mesure toutes les 5s
+  delay(5000);
 }
